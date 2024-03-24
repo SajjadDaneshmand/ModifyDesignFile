fd5fdf0385d996a90b0e10b5a9183756e3c7d2a9
unknown <Mr.SajjadDaneshmand@gmail.com>
2024-03-24 10:29:05 +0330

    Specify the ManageFiles class structure with its interface

---
+++ MDF/MDF/IManageFiles.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace WpfTestApp
+{
+    interface IManageFiles
+    {
+        void ReplaceInDesignFile(string filePath, bool toDot = true);
+        string OpenFile(string path);
+        void SaveFile(string path, string content);
+        string ReplaceToAll(string content, string input, string output);
+    }
+}
